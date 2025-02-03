d04da4415db341a6cac0ad0ac7bbf224c3757e50
utkarsh <utkarsh62@deloitte.com>
2025-02-03 13:20:44 +0530

    creating initial files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Body from './components/body';
+import Browse from './components/browse';
+import { RouterProvider } from 'react-router-dom';
 
 function App() {
+
+  const appRouter = createBrowserRouter([
+    {
+      path: '/',
+      element: <Body/>
+    },
+    {
+      path: '/browse',
+      element: <Browse/>
+    }
+  ]);
+
   return (
-    <div>Hello!</div>
+    <RouterProvider router={appRouter}>
+      <Body/>
+    </RouterProvider>
   );
 }
 
-export default App;
+export default App; 
---
+++ src/components/body.js
@@ -0,0 +1,13 @@
+import Header from "./header";
+import Login from "./login";
+
+const Body = () => {
+    return (
+        <div>
+            <Header/>
+            <Login/>
+        </div>
+    );
+}
+
+export default Body;
