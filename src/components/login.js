29e9101026a2f7c47d4187a88db8498ecb9ebb64
Utkarsh <utkarsh62@deloitte.com>
2025-02-09 21:27:31 +0530

    sign in, sign up form

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,15 @@
 import '../utils/login.css';
 import Header from "./header";
+import { useState } from 'react';
 
 const Login = () => {
+
+    const [isSignIn, setIsSignIn] = useState(true);
+
+    const toogleForm = () => {
+        setIsSignIn(!isSignIn);
+    }
+
     return (
         <div>
             <Header/>
@@ -9,18 +17,19 @@ const Login = () => {
                 <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_small.jpg" alt="background-img"></img>
                 <form className="login-form">
                 <div className='login-form-signin'>
-                    <h1>Sign In</h1>
+                    <h1>{isSignIn ? "Sign In" : "Sign Up"}</h1>
                     <input type="email" placeholder="Email"></input>
                     <input type="password" placeholder="Password"></input>
-                    <button>Sign In</button>
+                    <button>{isSignIn ? "Sign In" : "Sign Up"}</button>
                 </div>
-                <div className="login-form-signup">
-                    <span>New to Netflix? </span>
-                    <a href="/signup">Sign up now.</a>
-                </div>
+                {isSignIn ? 
+                    <div className="login-form-signup">
+                        <span>New to Netflix? <b onClick={toogleForm}>Sign up now</b></span>
+                    </div> : 
+                    <div className="login-form-signup"><span>Already a user? <b onClick={toogleForm}>Sign in now</b></span></div>
+                }
             </form>
             </div>
-           
         </div>
     );
 }
