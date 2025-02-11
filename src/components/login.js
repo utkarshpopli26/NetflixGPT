47867384d542231ab3195ec03caa5697e0e36033
Utkarsh <utkarsh62@deloitte.com>
2025-02-11 12:28:00 +0530

    firebase setup

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,24 @@
 import '../utils/login.css';
 import Header from "./header";
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { checkValidData } from '../utils/validate';
+
 
 const Login = () => {
 
+    const email = useRef(null);
+    const password = useRef(null);
+    const [ErrorMessage, setErrorMessage] = useState(null); 
     const [isSignIn, setIsSignIn] = useState(true);
 
-    const toogleForm = () => {
+    const ToogleForm = () => {
         setIsSignIn(!isSignIn);
+    }
+
+    const HandleSubmit = () => {
+        //validate form
+        const message = checkValidData(email.current.value, password.current.value);
+        setErrorMessage(message);
     }
 
     return (
@@ -15,18 +26,20 @@ const Login = () => {
             <Header/>
             <div className="login-container">
                 <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_small.jpg" alt="background-img"></img>
-                <form className="login-form">
+                <form onSubmit={(e) => e.preventDefault()} className="login-form">
                 <div className='login-form-signin'>
                     <h1>{isSignIn ? "Sign In" : "Sign Up"}</h1>
-                    <input type="email" placeholder="Email"></input>
-                    <input type="password" placeholder="Password"></input>
-                    <button>{isSignIn ? "Sign In" : "Sign Up"}</button>
+                    {!isSignIn && <input type='text' placeholder='Full Name'></input>}
+                    <input ref={email} type="email" placeholder="Email"></input>
+                    <input ref={password} type="password" placeholder="Password"></input>
+                    <p id="error">{ErrorMessage}</p>
+                    <button onClick={HandleSubmit}>{isSignIn ? "Sign In" : "Sign Up"}</button>
                 </div>
                 {isSignIn ? 
                     <div className="login-form-signup">
-                        <span>New to Netflix? <b onClick={toogleForm}>Sign up now</b></span>
+                        <span>New to Netflix? <b onClick={ToogleForm}>Sign up now</b></span>
                     </div> : 
-                    <div className="login-form-signup"><span>Already a user? <b onClick={toogleForm}>Sign in now</b></span></div>
+                    <div className="login-form-signup"><span>Already a user? <b onClick={ToogleForm}>Sign in now</b></span></div>
                 }
             </form>
             </div>
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdCQmmRxVXy4kcTWHyjNUM6BsFMmly55Y",
+  authDomain: "netflix-gpt-17a14.firebaseapp.com",
+  projectId: "netflix-gpt-17a14",
+  storageBucket: "netflix-gpt-17a14.firebasestorage.app",
+  messagingSenderId: "767291325040",
+  appId: "1:767291325040:web:8370bccf0d039cea2f52b5",
+  measurementId: "G-CVW07FLKW7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
